c7f155a4ecfbb4db950b294d952495a5b61658b5
jAzz-hub <joaogu.2001@gmail.com>
2022-11-16 17:53:15 -0300

    Adicionando um menu que insere um número de aviões específicos

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ int main()
         int op;
         Aeroporto aeroporto;
         int id, minutos_combustivel;
-        int contador = 0;
+        int contador_minutos = 0;
+        int avioes_chegando = 0;
+        
     //fim das variaveis locais
 
     //inicio do menu
@@ -28,28 +30,40 @@ int main()
             case 0:
                 cout<<"Saindo..."<<endl;
             break;
+
             case 1:
-                cout<<"Insira um id: "<<endl;
-                cin>>id;
+                cout<<"Insira aqui quantos aviões devem serão inseridos:"<<endl;
+                cin>>avioes_chegando;
 
-                cout<<"Inserir o tempo de combustível "<<endl;
-                cin>>minutos_combustivel;
 
-                aeroporto.inserir_aviao_na_aterrissagem(id,minutos_combustivel);
+                for(int contador_avioes_inseridos=0;contador_avioes_inseridos<avioes_chegando;contador_avioes_inseridos++)
+                {
+                    cout<<"Insira um id: "<<endl;
+                    cin>>id;
+
+                    cout<<"Inserir o tempo de combustível "<<endl;
+                    cin>>minutos_combustivel;
+
+
+                    aeroporto.inserir_aviao_na_aterrissagem(id,minutos_combustivel);
+                }
+
+
             break;
+
             default:
                 cout<<"Digite uma opção existente no menu!"<<endl;
             break;
         }
 
-        contador++;
+        contador_minutos++;
 
         //vai imprimir periodicamente dados dos avioẽs
-        if(contador==3)
+        if(contador_minutos==3)
         {
             aeroporto.remover_da_fila_aterrissagem();
             aeroporto.impressao_filas();
-            contador = 0;
+            contador_minutos = 0;
         }
 
     }while(op!=0);
